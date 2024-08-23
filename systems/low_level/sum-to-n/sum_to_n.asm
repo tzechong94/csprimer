a7e94b0c3838ab107a1fc630cfcbf5b223a188cd
tc94 <tze.chong1994@gmail.com>
2024-08-23 17:55:25 +0800

    refactor sum to n

--- systems/low_level/sum-to-n/sum_to_n.asm
+++ systems/low_level/sum-to-n/sum_to_n.asm
@@ -3,20 +3,29 @@
 
 section .text
 global sum_to_n
+; sum_to_n:
+; 	mov r9, rdi ; move argument into register
+
+; 	mov r10, 0 ; total
+; 	mov r8, 0 ; initial counter
+; 	cmp r9, 0;
+; 	je done
+; sum:
+; 	inc r8;
+
+; 	add r10, r8 ;
+; 	cmp r9, r8 ;
+
+; 	jg sum
+; done:
+; 	mov rax, r10
+; 	ret
+
+; int sum = N*(N+1)/2
+
 sum_to_n:
-	mov r9, rdi ; move argument into register
-
-	mov r10, 0 ; total
-	mov r8, 0 ; initial counter
-	cmp r9, 0;
-	je done
-sum:
-	inc r8;
-
-	add r10, r8 ;
-	cmp r9, r8 ;
-
-	jg sum
-done:
-	mov rax, r10
+	lea rax, [rdi + 1] ; load effective address to add 1 to rdi, reuslt stored in rax
+	; computes effective address second operate and stores it in the first
+	imul rax, rdi
+	sar rax, 1
 	ret
